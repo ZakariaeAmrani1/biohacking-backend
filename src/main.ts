4fd452c183b6c43ecc475a39f9031dc4acf372cf
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-08-16 14:38:39 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
   app.useGlobalPipes(
