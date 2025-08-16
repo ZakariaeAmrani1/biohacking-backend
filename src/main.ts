82e5d4e75eb6ab866a0671b8f62736c600482c85
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-08-16 14:22:40 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // or '*' for all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove unexpected fields
