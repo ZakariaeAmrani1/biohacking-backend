7c684cbeca2deb0def09136f8f11d651cdbc2de7
Zakariae Amrani <99502909+Alexo05@users.noreply.github.com>
2025-08-16 14:27:07 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // or '*' for all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*',
     credentials: true,
   });
   app.useGlobalPipes(
